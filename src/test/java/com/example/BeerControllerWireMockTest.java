4ff86a95e68732c94fc1657b8b56fe9e7765d0ed
Craig Erdmann <erds>
2018-06-13 12:50:44 -0500

    Added non-contract test for comparison

--- src/test/java/com/example/BeerControllerWireMockTest.java
+++ src/test/java/com/example/BeerControllerWireMockTest.java
@@ -1,39 +1,45 @@
 package com.example;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-/**
- * @author Marcin Grzejszczak
- */
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureStubRunner(workOffline = true, ids = "com.example:beer-api-producer:+:stubs:8090")
-public class BeerControllerTest extends AbstractTest {
+@WebMvcTest
+@AutoConfigureWireMock(port = 8090)
+public class BeerControllerWireMockTest extends AbstractTest {
 
   @Autowired
   MockMvc mockMvc;
   @Autowired
   BeerController beerController;
 
+
   @Test
   public void should_give_me_a_beer_when_im_old_enough() throws Exception {
+    String responseBody = "{\"status\":\"OK\"}";
+
+    WireMock.stubFor(WireMock.post(WireMock.urlEqualTo("/check"))
+        .willReturn(
+            WireMock.aResponse()
+                .withBody(responseBody)
+                .withStatus(200)
+                .withHeader("Content-Type", "application/json")
+        ));
+
     mockMvc.perform(MockMvcRequestBuilders.post("/beer")
         .contentType(MediaType.APPLICATION_JSON)
         .content(json.write(new Person("marcin", 22)).getJson()))
@@ -43,12 +49,23 @@ public class BeerControllerTest extends AbstractTest {
 
   @Test
   public void should_reject_a_beer_when_im_too_young() throws Exception {
+    String responseBody = "{\"status\":\"NOT_OK\"}";
+
+    WireMock.stubFor(WireMock.post(WireMock.urlEqualTo("/check"))
+        .willReturn(
+            WireMock.aResponse()
+                .withBody(responseBody)
+                .withStatus(200)
+                .withHeader("Content-Type", "application/json")
+        ));
+
     mockMvc.perform(MockMvcRequestBuilders.post("/beer")
         .contentType(MediaType.APPLICATION_JSON)
-        .content(json.write(new Person("marcin", 17)).getJson()))
+        .content(json.write(new Person("marcin", 15)).getJson()))
         .andExpect(status().isOk())
         .andExpect(content().string("GET LOST"));
   }
 }
+
 
 
